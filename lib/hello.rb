c6324931e9e27667fc1a1b6d860af3734c483ef8
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-13 00:10:55 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,7 +2,7 @@ def hello_t(array)
   
   i = 0
   while i < array.length
-   block_given? ? yield array[i] : puts "Hey! No block was given!"
+   block_given? ? yield array[i] : return "Hey! No block was given!"
   i += 1
   end
   array
